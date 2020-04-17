14b23b1aed47121b2aff7d7a1719deda87d99c3b
ygsong <ygsong@incruit.com>
2020-04-17 15:15:37 +0900

    create customView

---
+++ CustomView/CustomView/CustomView.swift
@@ -0,0 +1,25 @@
+//
+//  CustomView.swift
+//  CustomView
+//
+//  Created by Yeongeun Song on 2020/04/17.
+//  Copyright © 2020 Yeongeun Song. All rights reserved.
+//
+
+import UIKit
+
+protocol CustomViewDelegate {
+    func eventCustomView()
+}
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CustomView/CustomView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomView
+//
+//  Created by Yeongeun Song on 2020/04/17.
+//  Copyright © 2020 Yeongeun Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
