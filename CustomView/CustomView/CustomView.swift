3b9a683e04cdefb00e33332e595b54ebf6ff2aac
ygsong <ygsong@incruit.com>
2020-04-17 16:15:11 +0900

    complete customView

--- CustomView/CustomView/CustomView.swift
+++ CustomView/CustomView/CustomView.swift
@@ -12,14 +12,31 @@ protocol CustomViewDelegate {
     func eventCustomView()
 }
 
+@IBDesignable
 class CustomView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    public var delegate:CustomViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
     }
-    */
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+
+    private func commonInit(){
+        let bundle = Bundle.init(for: self.classForCoder)
+        let view = bundle.loadNibNamed("CustomView",
+                                       owner: self,
+                                       options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    @IBAction func tapBtn(_ sender: Any) {
+        self.delegate?.eventCustomView()
+    }
 }
--- CustomView/CustomView/ViewController.swift
+++ CustomView/CustomView/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var customView: CustomView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.customView.delegate = self
     }
+}
 
-
+extension ViewController: CustomViewDelegate {
+    func eventCustomView() {
+        print("customView event")
+    }
 }
 
